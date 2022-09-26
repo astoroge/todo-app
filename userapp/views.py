e1ab2d60ac85bec30357c337ae7c6211246809c8
astoroge <astoroge@gmail.com>
2022-09-26 08:25:01 +0800

    lesson 4

---
+++ userapp/views.py
@@ -0,0 +1,13 @@
+from rest_framework import mixins, viewsets
+from rest_framework.viewsets import ModelViewSet
+from .serializers import UserSerializer
+from .models import User
+
+
+class UserViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    
